6af4f936b0ee76d5d5c589a219f84a570d7f122e
Eduardo <eduguimara@gmail.com>
2023-06-09 22:15:36 -0300

    Project created

---
+++ src/main/java/com/devedu/springboot/models/ProductModel.java
@@ -0,0 +1,46 @@
+package com.devedu.springboot.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name ="TB_PRODUCTS")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public ProductModel setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ProductModel setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public ProductModel setValue(BigDecimal value) {
+        this.value = value;
+        return this;
+    }
+}
